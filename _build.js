9da9353c0ed72f08055df34913ec42fa0a0c5ab7
Giovani Rubim <giovanirubim.f@gmail.com>
2020-08-15 23:56:56 -0300

    Fix: funciona de qualquer diretorio; Nao precisa mais realizar copy

--- _build.js
+++ _build.js
@@ -1,9 +1,8 @@
 const exec = require('child_process').execSync;
 let [a, b, path] = process.argv;
 if (!path) exit(1);
-if (path[0] !== '"') {
-	path = `"${path}"`;
+if (path[0] === '"') {
+	path = path.replace(/^"|"$/g, '');
 }
-exec(`pkg -t node12-win-x64 ${ path }`, {stdio: 'inherit'});
-console.log('');
-exec(`move ${ path.replace(/\.js"$/, '.exe"') } C:\\mybin\\`);
+const dst = path.replace(/^([^\\]*\\)*((.+)(\.\w+)|(.*))$/, 'C:\\mybin\\$3$5.exe');
+exec(`pkg -t node12-win-x64 "${ path }" -o "${ dst }"`, {stdio: 'inherit'});
